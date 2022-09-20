4dd4433c057f7d02174888b796320f0b25ac4db4
Juan Velasco <jvelasco@iti.es>
2022-09-20 23:31:49 +0200

    Initial commit

---
+++ pre/webserver/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "juanvelascoaws"
+  region  = "eu-west-3"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    # Con esta configuración, junto con el "most_recent", siempre usaremos la última imagen generada.
+    values = ["app-symfony-packer-aws-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["545663651640"] # juanvelasco
+}
+
+module "webserver" {
+  source = "../../modules/webserver/"
+
+  instance_type = var.instance_type
+  key_pair_name = var.key_pair_name
+  ami_id        = data.aws_ami.ubuntu.id
+}
