eec3d0bce54712340a261b98ec81294904c041cd
Juan Velasco <jvelasco@iti.es>
2023-02-28 23:12:23 +0100

    Add pro environment

---
+++ pro/webserver/variables.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  description = "Instance type"
+  default = "t3.medium"
+}
+
+variable "key_pair_name" {
+  description = "Key pair name"
+  default = "aws_juanvelasco"
+}
